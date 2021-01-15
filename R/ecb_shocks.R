6533050da0b315d7804ffa5c317dc0af7db702ec
martindesktop <martin.baumgaertner1@gmail.com>
2021-01-15 13:47:45 +0100

    added option to remove local files

--- R/ecb_shocks.R
+++ R/ecb_shocks.R
@@ -7,6 +7,7 @@
 #' @param exclude_date vector of dates to exclude
 #' @param range Defines time window. Needs to be a vector of two dates
 #' @param crisis_date specify the starting date of the crisis for the rotation of the QE shocks
+#' @param remove_data should local data be removed after calculation?
 #'
 #' @return
 #'
@@ -17,11 +18,18 @@
 #' @export
 #' @importFrom
 ecb_shocks<-function(url="https://www.ecb.europa.eu/pub/pdf/annex/Dataset_EA-MPD.xlsx",
-                     path="",exclude_date=c("2001-09-17","2008-10-08","2008-11-06"),range=c("2001-12-31","2018-09-13"),crisis_date="2008-09-04"){
+                     path="",exclude_date=c("2001-09-17","2008-10-08","2008-11-06"),range=c("2001-12-31","2018-09-13"),crisis_date="2008-09-04",remove_data=T){
   download_hfd(url,path)
   pcw<-load_hfd(paste0(path,"pcw.csv"),exclude_date=exclude_date,range=range)
   prw<-load_hfd(paste0(path,"prw.csv"),exclude_date=exclude_date,range=range)
   release<-rotate(prw,crisis_date=crisis_date,window="release")
   conference<-rotate(pcw,crisis_date=crisis_date,window="conference")
+
+  if(remove_data==T){
+    file.remove(paste0(path,"prw.csv"))
+    file.remove(paste0(path,"pcw.csv"))
+    file.remove(paste0(path,"mew.csv"))
+  }
+
   return(full_join(release,conference,by="date"))
 }
