810ed4782ea9488d6c6aeffacc838b013ff379b6
martindesktop <martin.baumgaertner1@gmail.com>
2021-01-15 13:42:02 +0100

    path fix

--- R/ecb_shocks.R
+++ R/ecb_shocks.R
@@ -11,16 +11,16 @@
 #' @return
 #'
 #' @examples
-#' x<-ecb_shocks("https://www.ecb.europa.eu/pub/pdf/annex/Dataset_EA-MPD.xlsx",getwd(),
+#' x<-ecb_shocks("https://www.ecb.europa.eu/pub/pdf/annex/Dataset_EA-MPD.xlsx","",
 #'               exclude_date=c("2001-09-17","2008-10-08","2008-11-06"),
 #'               range=c("2001-12-31","2018-09-13"),crisis_date="2008-09-04")
 #' @export
 #' @importFrom
 ecb_shocks<-function(url="https://www.ecb.europa.eu/pub/pdf/annex/Dataset_EA-MPD.xlsx",
-                     path=getwd(),exclude_date=c("2001-09-17","2008-10-08","2008-11-06"),range=c("2001-12-31","2018-09-13"),crisis_date="2008-09-04"){
+                     path="",exclude_date=c("2001-09-17","2008-10-08","2008-11-06"),range=c("2001-12-31","2018-09-13"),crisis_date="2008-09-04"){
   download_hfd(url,path)
-  pcw<-load_hfd(paste0(path,"/pcw.csv"),exclude_date=exclude_date,range=range)
-  prw<-load_hfd(paste0(path,"/prw.csv"),exclude_date=exclude_date,range=range)
+  pcw<-load_hfd(paste0(path,"pcw.csv"),exclude_date=exclude_date,range=range)
+  prw<-load_hfd(paste0(path,"prw.csv"),exclude_date=exclude_date,range=range)
   release<-rotate(prw,crisis_date=crisis_date,window="release")
   conference<-rotate(pcw,crisis_date=crisis_date,window="conference")
   return(full_join(release,conference,by="date"))
